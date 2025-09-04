b8bd1aac39a44050ead43f98a3265539800e4cbd
Hacker01russ <dehup182022@yandex.com>
2025-09-04 10:46:32 +0300

    Update Application.java

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -5,6 +5,5 @@ public class Application {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
         PostController controller = ctx.getBean(PostController.class);
-        // use the controller here or pass it where needed
     }
 }
