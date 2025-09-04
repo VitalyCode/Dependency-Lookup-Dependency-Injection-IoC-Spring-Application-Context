102bcfcea9462ab6f16144789d61bb47b0aef924
Hacker01russ <dehup182022@yandex.com>
2025-09-04 10:02:52 +0300

    oneCommit

---
+++ src/main/java/repository/PostRepository.java
@@ -0,0 +1,44 @@
+package repository;
+
+import model.Post;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class PostRepository {
+
+    private List<Post> posts = new ArrayList<>();
+    private AtomicLong idCounter = new AtomicLong(0);
+
+    public List<Post> all() {
+        return Collections.unmodifiableList(posts);
+    }
+
+    public Optional<Post> getById(long id) {
+        return posts.stream()
+                .filter(p -> p.getId() == id)
+                .findFirst();
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            post.setId(idCounter.incrementAndGet());
+            posts.add(post);
+        } else {
+            Optional<Post> existingPost = getById(post.getId());
+            if (existingPost.isPresent()) {
+                existingPost.get().setContent(post.getContent());
+            } else {
+                return null;
+            }
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        posts.removeIf(p -> p.getId() == id);
+    }
+}
---
+++ src/main/java/service/PostService.java
@@ -0,0 +1,31 @@
+package service;
+
+import model.Post;
+import repository.PostRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class PostService {
+    private final PostRepository repository;
+
+    public PostService(PostRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Post> all() {
+        return repository.all();
+    }
+
+    public Optional<Post> getById(long id) {
+        return repository.getById(id);
+    }
+
+    public Post save(Post post) {
+        return repository.save(post);
+    }
+
+    public void removeById(long id) {
+        repository.removeById(id);
+    }
+}
